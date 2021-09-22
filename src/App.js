777043afcfa6a96808041e11fddf42bd735624e6
shaikat <shaikatpal56@gmail.com>
2021-09-22 11:11:26 +0600

    Digital Clock Using react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react'
 function App() {
+
+  let newTime = new Date().toLocaleTimeString()
+
+  const [inTime, cTime] = useState(newTime)
+
+  const updateTime = () => {
+    newTime = new Date().toLocaleTimeString()
+    cTime(newTime)
+  }
+
+  setInterval(updateTime, 1000)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <h1>{ inTime }</h1>
     </div>
+    </>
   );
 }
 
